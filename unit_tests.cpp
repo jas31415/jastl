e06a89b14dc9d39749ff867c51b98961527bfd86
jas31415 <yourdankestmeme@gmail.com>
2025-05-16 02:09:50 +0200

    added very important comments to code =]

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,6 +1,17 @@
 /*
 	unit-tests.cpp is the file where unit tests are performed with the wrapper classes
 	the main function is therefore also defined here
+
+	in theory, i'd like the unit tests to operate as below
+		Consider the following:
+    		test -a                       performs unit tests on all headers available in this c++ standard version
+   			test -e                       performs unit tests on all headers regardless of availability
+    		test <header1 header2 ...>    performs unit tests on each header listed
+    		list -a                       lists all headers available in this c++ standard version
+    		list -e                       lists all headers regardless of availability
+    		std                           shows the c++ standard version currently being used
+    		jastl                         shows the jastl version currently being used
+    		help                          shows this menu
 */
 
 #include <iostream>
@@ -9,11 +20,8 @@
 
 int main()
 {
-	Example* pExample( new Example );
-	delete pExample;
-	Example example;
-
-	std::cout << __cplusplus << std::endl;
+	std::cout << "Press any key to exit the terminal." << std::endl;
+	std::cin.get();
 
 	return 0;
 }
